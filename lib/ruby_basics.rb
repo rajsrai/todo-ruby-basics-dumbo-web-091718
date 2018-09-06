8d9e1c64b87a3c7ccb5f8cfd8c713ae4aa828a1c
Rajvir Rai <rsr1992@gmail.com>
2018-09-06 15:09:32 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  return 12/2
 end
 
 def assign_variable(value)
-  
+  value = "Raj"
 end
 
-def argue
-  
+def argue()
+  return
 end
 
-def greeting
-
+def greeting(hello, name)
+return greeting
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+  return phrase
 end
 
-def pizza_party
-
+def pizza_party(cheese)
+  cheese= "cheese"
+  cheese
 end
